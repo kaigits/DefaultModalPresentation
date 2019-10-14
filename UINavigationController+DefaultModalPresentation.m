d51e2059fbdee2c5bbb853466c315cd5d115af7f
kaigits <511892596@qq.com>
2019-10-14 15:49:14 +0800

    Update UINavigationController+DefaultModalPresentation.m

--- UINavigationController+DefaultModalPresentation.m
+++ UINavigationController+DefaultModalPresentation.m
@@ -6,7 +6,33 @@
 
 
 #import "UINavigationController+DefaultModalPresentation.h"
-#import "NSObject+Swizzle.h"
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@interface NSObject (DMPSwizzle)
+
++ (void)swizzleInstanceSelector:(SEL)originalSelector withNewSelector:(SEL)newSelector;
+
+@end
+
+@implementation NSObject (DMPSwizzle)
+
++ (void)swizzleInstanceSelector:(SEL)originalSelector withNewSelector:(SEL)newSelector {
+    Method originalMethod = class_getInstanceMethod(self, originalSelector);
+    Method newMethod = class_getInstanceMethod(self, newSelector);
+    
+    BOOL methodAdded = class_addMethod([self class], originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
+    if (methodAdded) {
+        class_replaceMethod([self class], newSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    }
+    else {
+        method_exchangeImplementations(originalMethod, newMethod);
+    }
+}
+
+@end
+
+
 @implementation UINavigationController (DefaultModalPresentation)
 + (void)load{
     if (@available(iOS 13.0, *)) {
